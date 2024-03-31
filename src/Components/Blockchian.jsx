74f14b0a28eca81fa9b54552515dea2f4659d9cd
daylightcoderj6 <jananibharathi6@gmail.com>
2024-03-31 20:17:32 +0530

    added cardDatas in Blockchain.jsx

--- src/Components/Blockchian.jsx
+++ src/Components/Blockchian.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Card1 from './Cards/Card1'
 
 const Blockchian = () => {
+  const cardDatas = [
+    {id:"1",img:"./src/assets/BC1.jpeg",cardTitle:"A Guide To Learning Blockchain Technology From Scratch",cardBody:"By Lukesh S" },
+    {id:"2",img:"./src/assets/BC2.webp",cardTitle:"Non-Coding Jobs in Blockchain",cardBody:"By Jaishree Tomar" },
+    {id:"3",img:"./src/assets/BC3.webp",cardTitle:"Best Books to Learn Blockchain",cardBody:"By Meghana D" }
+  ]
   return (
-    <div>Blockchian</div>
+    <div className='container'>
+        <div className="row">
+            {cardDatas.map(data=>(
+              <div className='col-sm-12 col-md-6 col-lg-4 justify-content-center margin' key={data.id}>
+              <Card1
+              {...data}/>
+              </div>
+            ))}
+        </div>
+    </div>
   )
 }
 
--- src/Components/Cards/Card1.jsx
+++ src/Components/Cards/Card1.jsx
@@ -4,7 +4,7 @@ import './Card1.css'
 const Card1 = (props) => {
     return (
         <div>
-            <div className="card" style={{ width: '18rem', marginTop: "20px" }}>
+            <div className="card" style={{ width: '18rem', marginTop: "30px" }}>
                 <img src={props.img} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{props.cardTitle}</h5>
